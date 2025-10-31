01d6fb6775c7b85614cc8c6bf8de93efc7915299
程浦江 <cpj2895038949@hotmail.com>
2025-10-31 23:05:50 +0800

    feat: 文档整理

--- .vitepress/themeConfig.ts
+++ .vitepress/themeConfig.ts
@@ -1,27 +1,40 @@
 import { type DefaultTheme } from 'vitepress'
 
 const Inbox: DefaultTheme.NavItemWithLink[] = [
-  { text: 'Main', link: '/docs/main' },
-  { text: 'Demo', link: '/docs/demo' },
-  { text: '时间日期', link: '/docs/main/dayjs' },
-  { text: '图表', link: '/docs/main/visual/index.md' },
-  { text: '响应式布局', link: '/docs/main/响应式布局方案' },
-  { text: 'Element UI', link: '/docs/main/element-ui' },
-  { text: 'HTML+CSS', link: '/docs/css' },
-  { text: 'JavaScript', link: '/docs/javascript' }
+  { text: 'Demo', link: '/docs/demo/index.md' },
+  { text: '时间日期', link: '/docs/dayjs/index.md' },
+  { text: '图表', link: '/docs/charts/index.md' },
+  { text: 'Element UI', link: '/docs/element-ui/index.md' },
+  { text: 'HTML+CSS', link: '/docs/css/index.md' },
+  { text: 'JavaScript', link: '/docs/javascript/index.md' }
 ]
 
 /**
  * 侧边栏
  */
-export const Sidebar: DefaultTheme.SidebarMulti = {
-  '/docs/inbox/': [
+export const Sidebar: DefaultTheme.Sidebar = {
+  '/docs/': [
     {
       text: '收件箱',
+      collapsed: true,
       items: Inbox
     }
   ]
 }
+
+// export const Sidebar: DefaultTheme.Sidebar = [
+//   {
+//     text: '收件箱',
+//     collapsed: true,
+//     items: Inbox
+//   },
+//   {
+//     text: '收件箱',
+//     collapsed: true,
+//     items: Inbox
+//   }
+// ]
+
 
 /**
  * 导航栏
