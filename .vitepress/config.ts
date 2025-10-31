9608f71a1b831bba2727be0040473d1ee9a466b8
chengpujiang <chengpujiang@jtexpress.com>
2025-10-31 13:15:24 +0800

    2.0第一版

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,6 +3,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vitepress'
 import { groupIconMdPlugin, groupIconVitePlugin } from 'vitepress-plugin-group-icons'
+import { Nav, SidebarCssTutorial, SidebarDevLog, SidebarJsTutorial } from './themeConfig'
 
 
 // https://vitepress.dev/reference/site-config
@@ -24,6 +25,65 @@ export default defineConfig({
     // },
     config(md) {
       md.use(groupIconMdPlugin)
+    }
+  },
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: Nav,
+    sidebar: {
+      '/dev-log/': SidebarDevLog,
+      '/css-tutorial/': SidebarCssTutorial,
+      '/js-tutorial/': SidebarJsTutorial
+    },
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+    docFooter: {
+      prev: '上一页',
+      next: '下一页'
+    },
+    outline: {
+      label: '页面导航'
+    },
+    lastUpdated: {
+      text: '最后更新于'
+    },
+    notFound: {
+      title: '页面未找到',
+      quote:
+        '但如果你不改变方向，并且继续寻找，你可能最终会到达你所前往的地方。',
+      linkLabel: '前往首页',
+      linkText: '带我回首页'
+    },
+    langMenuLabel: '多语言',
+    returnToTopLabel: '回到顶部',
+    sidebarMenuLabel: '菜单',
+    darkModeSwitchLabel: '主题',
+    lightModeSwitchTitle: '切换到浅色模式',
+    darkModeSwitchTitle: '切换到深色模式',
+    skipToContentLabel: '跳转到内容',
+    search: {
+      provider: 'local',
+      options: {
+        locales: {
+          zh: {
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档'
+              },
+              modal: {
+                noResultsText: '无法找到相关结果',
+                resetButtonTitle: '清除查询条件',
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换'
+                }
+              }
+            }
+          }
+        }
+      }
     }
   },
   vite: {
@@ -55,7 +115,7 @@ export default defineConfig({
       }),
       AutoImport({
         imports: ['vue'],
-        dts: 'auto-import.d.ts',
+        dts: '../auto-import.d.ts',
         eslintrc: {
           enabled: true
         }
--- .vitepress/themeConfig.ts
+++ .vitepress/themeConfig.ts
@@ -16,11 +16,10 @@ const JsTutorial: DefaultTheme.NavItemWithLink[] = [
   { text: 'js-tutorial', link: '/js-tutorial/' },
 ]
 
-const Nav: DefaultTheme.NavItem[] = [
+export const Nav: DefaultTheme.NavItem[] = [
   {
     text: 'DevLog',
     items: DevLog,
-    activeMatch: '^/dev-log/'
   },
   {
     text: 'CssTutorial',
@@ -34,7 +33,7 @@ const Nav: DefaultTheme.NavItem[] = [
   }
 ]
 
-const SidebarDevLog: DefaultTheme.SidebarItem[] = [
+export const SidebarDevLog: DefaultTheme.SidebarItem[] = [
   {
     text: 'DevLog',
     items: DevLog
@@ -45,78 +44,16 @@ const SidebarDevLog: DefaultTheme.SidebarItem[] = [
   }
 ]
 
-const SidebarCssTutorial: DefaultTheme.SidebarItem[] = [
+export const SidebarCssTutorial: DefaultTheme.SidebarItem[] = [
   {
     text: 'CssTutorial',
     items: CssTutorial
   }
 ]
 
-const SidebarJsTutorial: DefaultTheme.SidebarItem[] = [
+export const SidebarJsTutorial: DefaultTheme.SidebarItem[] = [
   {
     text: 'JsTutorial',
     items: JsTutorial
   }
 ]
-
-export default defineAdditionalConfig({
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: Nav,
-    sidebar: {
-      '/dev-log/': SidebarDevLog,
-      '/css-tutorial/': SidebarCssTutorial,
-      '/js-tutorial/': SidebarJsTutorial
-    },
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ],
-    docFooter: {
-      prev: '上一页',
-      next: '下一页'
-    },
-    outline: {
-      label: '页面导航'
-    },
-    lastUpdated: {
-      text: '最后更新于'
-    },
-    notFound: {
-      title: '页面未找到',
-      quote:
-        '但如果你不改变方向，并且继续寻找，你可能最终会到达你所前往的地方。',
-      linkLabel: '前往首页',
-      linkText: '带我回首页'
-    },
-    langMenuLabel: '多语言',
-    returnToTopLabel: '回到顶部',
-    sidebarMenuLabel: '菜单',
-    darkModeSwitchLabel: '主题',
-    lightModeSwitchTitle: '切换到浅色模式',
-    darkModeSwitchTitle: '切换到深色模式',
-    skipToContentLabel: '跳转到内容',
-    search: {
-      provider: 'local',
-      options: {
-        locales: {
-          zh: {
-            translations: {
-              button: {
-                buttonText: '搜索文档',
-                buttonAriaLabel: '搜索文档'
-              },
-              modal: {
-                noResultsText: '无法找到相关结果',
-                resetButtonTitle: '清除查询条件',
-                footer: {
-                  selectText: '选择',
-                  navigateText: '切换'
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-})
