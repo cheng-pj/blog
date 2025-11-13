e26a1b17cb953fa9513db6a21280dc5074d0a85d
程浦江 <cpj2895038949@hotmail.com>
2025-11-13 22:56:53 +0800

    feat: 新增了一个文件

--- docs/example/jsx/components/Jsx1.vue
+++ docs/example/jsx/components/Jsx1.vue
@@ -1,14 +1,14 @@
-<script setup lang="jsx">
-const GridItem = () => {
+<script setup lang="tsx">
+import Jsx2 from './Jsx2'
+
+const Jsx1 = () => {
   return (
-    <div>
-      我是一个jsx中的div
-      <p>我是一个p结点</p>
-    </div>
+    <div>我是一个jsx中的div</div>
   )
 }
 </script>
 
 <template>
-  <GridItem/>
+  <Jsx1/>
+  <Jsx2/>
 </template>
---
+++ docs/example/jsx/components/Jsx2.tsx
@@ -0,0 +1,8 @@
+export default defineComponent({
+  name: 'Jsx2',
+  render() {
+    return (
+      <div>这段来自tsx</div>
+    )
+  }
+})
